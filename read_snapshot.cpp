sbird/fake_spectra

commit dfd02d3058c20a92bc1a24f96318efbf12e60eff

Restore MASS to being a proper array rather than a single value, since AREPO may have varying masses

read_snapshot.cpp
@@ -50,12 +50,14 @@ extern "C" int load_snapshot(char *fname, pdata *P,
   snap.GetBlock("VEL ",(*P).Vel,NumPart,0, (1<<N_TYPE)-1-(1<<PARTTYPE));
   /* Particles masses  */
   printf("Reading mass and temp...\n");
-  /*DANGER: This assumes that we are reading baryons and that masses are constant*/
-  if(snap.GetHeader().mass[PARTTYPE])
-        (*P).Mass = snap.GetHeader().mass[PARTTYPE];
+  if(snap.GetHeader().mass[PARTTYPE]){
+        int i;
+        for(i=0; i< NumPart;i++)
+           (*P).Mass[i] = snap.GetHeader().mass[PARTTYPE];
+  }
   else
-        snap.GetBlock("MASS",&((*P).Mass),1,0,0);
-  (*omegab) = (*P).Mass/((*P).Mass+snap.GetHeader().mass[1])*snap.GetHeader().Omega0;
+        snap.GetBlock("MASS",&((*P).Mass),NumPart,0, (1<<N_TYPE)-1-(1<<PARTTYPE));
+  (*omegab) = (*P).Mass[0]/((*P).Mass[0]+snap.GetHeader().mass[1])*snap.GetHeader().Omega0;
   /*Seek past the last masses*/
   if(PARTTYPE == 0)
     { 
@@ -96,7 +98,7 @@ extern "C" int load_snapshot(char *fname, pdata *P,
     }
   printf("P[%d].Pos = [%g %g %g]\n", 0, (*P).Pos[0], (*P).Pos[1],(*P).Pos[2]);
   printf("P[%d].Vel = [%g %g %g]\n", 0, (*P).Vel[0], (*P).Vel[1],(*P).Vel[2]);
-  printf("P[%ld].Mass = %e Ω_B=%g\n\n", NumPart, (*P).Mass,(*omegab));
+  printf("P[%ld].Mass = %e Ω_B=%g\n\n", NumPart, (*P).Mass[0],(*omegab));
   printf("P[%ld].U = %f\n\n", NumPart, (*P).U[NumPart-1]);
   printf("P[%ld].Ne = %e\n", NumPart, (*P).Ne[NumPart-1]);
   printf("P[%ld].NH0 = %e\n", NumPart, (*P).NH0[NumPart-1]);
@@ -138,7 +140,7 @@ extern "C" int alloc_parts(pdata* P, int np)
 {
     return ((*P).Vel=(float *)malloc(np*3*sizeof(float))) &&
     ((*P).Pos=(float *)malloc(np*3*sizeof(float))) &&
-/*     ((*P).Mass=malloc(np*sizeof(float))) && */
+     ((*P).Mass=(float *) malloc(np*sizeof(float))) &&
     ((*P).U=(float *)malloc(np*sizeof(float))) &&
     ((*P).NH0=(float *)malloc(np*sizeof(float))) &&
     ((*P).Ne=(float *)malloc(np*sizeof(float))) &&
@@ -152,7 +154,7 @@ extern "C" void free_parts(pdata* P)
 {
     free((*P).Vel);
     free((*P).Pos);
-/*     free((*P).Mass); */
+    free((*P).Mass);
     free((*P).U);
     free((*P).NH0);
     free((*P).Ne);

extract_spectra.c
@@ -329,7 +329,7 @@ void SPH_Interpolation(double * rhoker_H, interp * H1, interp * He2, const int P
 	        
 	        kernel *= hinv3; 
 
-	        kernel *= (*P).Mass; /* kg (kpc)^-3 */
+	        kernel *= (*P).Mass[i]; /* kg (kpc)^-3 */
 	        velker = vr * kernel; /* kg (kpc)^-3 * km s^-1 */
 	        temker = temp * kernel; /* kg (kpc)^-3 * K */
                 /*Only one thread can update the global memory at a time.

global_vars.h
@@ -10,7 +10,7 @@ struct particle_data
 {
   float *Pos;
   float *Vel;
-  float Mass;
+  float *Mass;
   float *U, *NH0, *Ne, *h;
 #ifdef HELIUM
   float *NHep;

commit 1e069779ad638d3fbbf499f9815ad4b2edfff010

Fix bug caused by dfd02d3058c20a92bc1a24f96318efbf12e60eff where we were reading the MASS array wrongly caused by trying to skip nonexistent DM particles

read_snapshot.cpp
@@ -50,13 +50,22 @@ extern "C" int load_snapshot(char *fname, pdata *P,
   snap.GetBlock("VEL ",(*P).Vel,NumPart,0, (1<<N_TYPE)-1-(1<<PARTTYPE));
   /* Particles masses  */
   printf("Reading mass and temp...\n");
-  if(snap.GetHeader().mass[PARTTYPE]){
-        int i;
-        for(i=0; i< NumPart;i++)
+  if(snap.GetHeader().mass[PARTTYPE])
+        for(int i=0; i< NumPart;i++)
            (*P).Mass[i] = snap.GetHeader().mass[PARTTYPE];
+  else{
+        /*Set up types to skip; skip all types not in the mass array*/
+        int skip = (1<<N_TYPE)-1-(1<<PARTTYPE);
+        for(int i=0; i< N_TYPE; i++)
+           if(snap.GetHeader().mass[i])
+                   skip-=1<<i;
+        snap.GetBlock("MASS",(*P).Mass,NumPart,0, skip);
   }
-  else
-        snap.GetBlock("MASS",(*P).Mass,NumPart,0, (1<<N_TYPE)-1-(1<<PARTTYPE));
+  for(int i=0; i< NumPart;i++)
+  if ((*P).Mass[i] != (*P).Mass[0]){
+        fprintf(stderr, "i=%d N = %ld Mass change: %g\n",i,NumPart, (*P).Mass[i]);
+        exit(0);
+  }
   (*omegab) = (*P).Mass[0]/((*P).Mass[0]+snap.GetHeader().mass[1])*snap.GetHeader().Omega0;
   /*Seek past the last masses*/
   if(PARTTYPE == 0)