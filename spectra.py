8ed02cd00bb07ba30ac31c9d2435e839dddca32e
Simeon Bird <spb@ias.edu>
2013-01-29 15:41:54 -0500

    Various fixes

--- spectra.py
+++ spectra.py
@@ -1,12 +1,34 @@
-"""Python module for generating fake spectra from an N-body catalogue."""
+"""Python module for generating fake spectra from an N-body catalogue.
+
+Note in Arepo we have GFM_Metals and GFM_Metallicity.
+
+GFM_Metallicity is the total mass in species not H or He
+per unit gas mass (and is used for cooling).
+
+GFM_Metals is a 9-component array of species:
+H, He, C, N, O, Ne, Mg, Si, Fe
+
+Because these are not all the species, GFM_Metals will not sum to 1
+and sum(GFM_Metals[2:])  < GFM_Metallicity
+
+However, it should be true that
+1- sum(GFM_Metals[:]) + GFM_Metals[2:] = GFM_Metallicity
+
+Also note that there is some instability at very low metallicities - the code will often return +-1e-20.
+"""
+
 
 import numpy as np
 import hsml
+import math
+import h5py
+import convert_cloudy
+import line_data
 from _spectra_priv import _SPH_Interpolate
 
 def SPH_Interpolate(data, los_table, nbins, box):
     """Interpolate particles to lines of sight, calculating density, temperature and velocity
-    of various species (TODO: only hydrogen now) along the line of sight.
+    of various species along the line of sight.
 
     This is a wrapper which calls the C function.
     Arguments:
@@ -29,6 +51,8 @@ def SPH_Interpolate(data, los_table, nbins, box):
     ne = np.array(data["ElectronAbundance"],dtype=np.float32)
     try:
         metals = np.array(data["GFM_Metals"],dtype=np.float32)[:,2:]
+        #Deal with floating point roundoff - metals will sometimes be negative
+        metals[np.where(np.abs(metals) < 1e-10)] = 0
     except IOError:
         metals = np.array()
     hh = np.array(hsml.get_smooth_length(data),dtype=np.float32)
@@ -42,90 +66,71 @@ def SPH_Interpolate(data, los_table, nbins, box):
 #Speed of light
 C = 2.99e8
 #Boltzmann constant
-KBOLTZ =1.3806504e-23
-
-class line:
-    sigma_X
-    gamma_X
-    lambda_X
-    m_X
-
-"""Note in Arepo we have GFM_Metals and GFM_Metallicity.
-
-GFM_Metallicity is the total mass in species not H or He
-per unit gas mass (and is used for cooling).
-
-GFM_Metals is a 9-component array of species:
-H, He, C, N, O, Ne, Mg, Si, Fe
-
-Because these are not all the species, GFM_Metals will not sum to 1
-and sum(GFM_Metals[2:])  < GFM_Metallicity
-
-However, it should be true that
-1- sum(GFM_Metals[:]) + GFM_Metals[2:] = GFM_Metallicity
-"""
+KBOLTZ = 1.3806504e-23
 
 def compute_absorption(xbins, rho, vel, temp, line):
-    """Computes the absorption spectrum (tau (u) ) from a binned set of interpolated densities, velocities and temperatures.
+    """Computes the absorption spectrum (tau (u) ) from a binned set of interpolated
+    densities, velocities and temperatures.
     xbins are the positions of each bin along the sightline. A good default is
     xbins = np.range(0,NBINS)*Box/NBINS
     Adapted from ComputeAbsorption in flux_extractor.
     sigma_X is the cross-section for this transition.
     Optical depth is given by:
-        \\tau (u) = \sigma_X c / H(z) \int_\infty^\infty n_x(x) V( u - x - v_pec, b(x) ) dx
-        where V is the Voigt profile, b(x)^2 = 2k_B T /m_x c^2 is the velocity dispersion.
-        and v_pec is the peculiar velocity.
-        """
-#  const double H0 = 1.0e5/MPC; /* 100kms^-1Mpc^-1 in SI */
-#    /* Critical matter/energy density at z = 0.0 */
-#  const double rhoc = 3.0 * (H0*h100)*(H0*h100) / (8.0 * M_PI * GRAVITY); /* kgm^-3 */
-#  /* Mean hydrogen mass density of the Universe */
-#  const double critH = (rhoc * omegab * XH) / (atime*atime*atime); /* kgm^-3*/
-#  /* Conversion factors from internal units */
-#  const double rscale = (KPC*atime)/h100;   /* convert length to m */
-#  const double vscale = sqrt(atime);        /* convert velocity to kms^-1 */
-#  const double mscale = (1.0e10*SOLAR_MASS)/h100; /* convert mass to kg */
-#  const double escale = 1.0e6;           /* convert energy/unit mass to J kg^-1 */
-#  const double tscale = ((GAMMA-1.0) * HMASS * PROTONMASS * escale ) / BOLTZMANN; /* convert (with mu) T to K */
-#  /*const double hscale = rscale * 0.5;*/ /* Note the factor of 0.5 for this kernel definition */
-#  /*    Calculate the length scales to be used in the box */
-#  const double dzgrid   = (box100) / (double)NBINS; /* bin size (kpc) */
-#  const double vmax = box100 * Hz * rscale/ MPC; /* box size (kms^-1) */
-#  const double vmax2 = vmax/2.0; /* kms^-1 */
-#  const double dvbin = vmax / (double)NBINS; /* bin size (kms^-1) */
+    tau (u) = sigma_X c / H(z) int_infty^infty n_x(x) V( u - x - v_pec, b(x) ) dx
+    where V is the Voigt profile, b(x)^2 = 2k_B T /m_x c^2 is the velocity dispersion.
+    and v_pec is the peculiar velocity.
+    """
+    nbins = np.size(xbins)
+    #  const double H0 = 1.0e5/MPC; /* 100kms^-1Mpc^-1 in SI */
+    #    /* Critical matter/energy density at z = 0.0 */
+    #  const double rhoc = 3.0 * (H0*h100)*(H0*h100) / (8.0 * M_PI * GRAVITY); /* kgm^-3 */
+    #  /* Mean hydrogen mass density of the Universe */
+    #  const double critH = (rhoc * omegab * XH) / (atime*atime*atime); /* kgm^-3*/
+    #  Conversion factors from internal units
+    rscale = (KPC*atime)/h100  # convert length to m
+    #  const double vscale = sqrt(atime);        /* convert velocity to kms^-1 */
+    #  const double mscale = (1.0e10*SOLAR_MASS)/h100; /* convert mass to kg */
+    #  const double escale = 1.0e6;           /* convert energy/unit mass to J kg^-1 */
+    #  const double tscale = ((GAMMA-1.0) * HMASS * PROTONMASS * escale ) / BOLTZMANN; /* convert (with mu) T to K */
+    #  /*const double hscale = rscale * 0.5;*/ /* Note the factor of 0.5 for this kernel definition */
+    #  /*    Calculate the length scales to be used in the box */
+    dzgrid   = box100 / nbins  # bin size (kpc)
+    #  const double vmax = box100 * Hz * rscale/ MPC; /* box size (kms^-1) */
+    #  const double vmax2 = vmax/2.0; /* kms^-1 */
+    #  const double dvbin = vmax / (double)NBINS; /* bin size (kms^-1) */
 
-#   const double sigma_Lya_H1  = sqrt(3.0*M_PI*SIGMA_T/8.0) * LAMBDA_LYA_H1  * FOSC_LYA;
-  #Optical depth prefactor
-  tau_prefac = rscale*line.sigma_X*C*dzgrid/sqrt(math.pi)
-  # If there are no particles in this bin, rhoker will be zero.
-  # In this case, we set temp and veloc arbitrarily to one,
-  # to avoid nans propagating. Zero rho will imply zero absorption
-  # anyway.
-  #ind = np.where(rho > 0)
-  #veloc[ind]  *= vscale/rho[ind] # HI weighted km s^-1
-  #temp[ind]   *= tscale/rho[ind # HI weighted K
-  #rho *= mscale/rscale**3   #Put rho in m units
-  #ind = np.where(rho <=0)
-  #veloc[ind]=1
-  #temp[ind]=1
-  #Compute the spectra
-  tau = np.zeros(NBINS)
-  #Total velocity of each bin relative to start of box: H x + v
-  u_H  = Hubble(z) /MPC *xbins + vel
-  vmax  = Hubble(z) /MPC *xbins[-1]
-  for i in xrange(0, NBINS):
-      #Velocity of this cloud relative to this bin, wrapped periodically
-      vdiff  = fabs(xbins[i] - u_H1)
-      ind = np.where(vdiff > vmax)
-      vdiff[ind] = vmax - vdiff
-      bb = vel_disp(temp, line.m_x)
-      #x for the Voigt profile is (c/b) (lambda/lambda_i-1), so this is x/c
-      x = (vdiff/bb
-      #a for the Voigt profile
-      aa = line.gamma_X*line.lambda_X/(4.0*math.pi*bb)
-      prof = voigt(aa, x)
-      tau[i] = A_H1 * np.sum(rho*profile/bb/m_x)
-  return tau
+    #   const double sigma_Lya_H1  = sqrt(3.0*M_PI*SIGMA_T/8.0) * LAMBDA_LYA_H1  * FOSC_LYA;
+    #Optical depth prefactor
+    tau_prefac = rscale*line.sigma_X*C*dzgrid/np.sqrt(math.pi)
+    # If there are no particles in this bin, rhoker will be zero.
+    # In this case, we set temp and veloc arbitrarily to one,
+    # to avoid nans propagating. Zero rho will imply zero absorption
+    # anyway.
+    #ind = np.where(rho > 0)
+    #veloc[ind]  *= vscale/rho[ind] # HI weighted km s^-1
+    #temp[ind]   *= tscale/rho[ind # HI weighted K
+    #rho *= mscale/rscale**3   #Put rho in m units
+    #ind = np.where(rho <=0)
+    #veloc[ind]=1
+    #temp[ind]=1
+    #Compute the spectra
+    tau = np.zeros(nbins)
+    #Total velocity of each bin relative to start of box: H x + v
+    u_H  = Hubble(z) /MPC *xbins + vel
+    vmax  = Hubble(z) /MPC *xbins[-1]
+    for i in xrange(0, nbins):
+        #Velocity of this cloud relative to this bin, wrapped periodically
+        vdiff  = np.abs(xbins[i] - u_H1)
+        ind = np.where(vdiff > vmax)
+        vdiff[ind] = vmax - vdiff
+        bb = vel_disp(temp, line.m_x)
+        #x for the Voigt profile is (c/b) (lambda/lambda_i-1), so this is x/c
+        x = vdiff/bb
+        #a for the Voigt profile
+        aa = line.gamma_X*line.lambda_X/(4.0*math.pi*bb)
+        prof = voigt(aa, x)
+        tau[i] = A_H1 * np.sum(rho*profile/bb/m_x)
+    return tau
 
 def vel_disp(temp, m_x):
     """Velocity dispersion (b param) as a function of temperature and particle mass"""
@@ -134,16 +139,16 @@ def vel_disp(temp, m_x):
 def voigt(a, x):
     """Voigt profile (Tepper-Garcia, 2006: astro-ph/0602124)"""
     x2 = x*x
-    T1 = exp(-x2)
+    T1 = np.exp(-x2)
     T2 = 1.5/x2
     if x2 < 1.0e-10:
         profile = T1
     else:
-        profile = T1 - a/sqrt(math.pi)/x2*(T1*T1*(4.0*x2**2 + 7.0*x2 + 4.0 + T2) - T2 -1.0)
+        profile = T1 - a/np.sqrt(math.pi)/x2*(T1*T1*(4.0*x2**2 + 7.0*x2 + 4.0 + T2) - T2 -1.0)
     return profile
 
 #Stolen from yt
-def voigt(a,u):
+def voigtyt(a,u):
     """
     NAME:
         VOIGT
@@ -186,14 +191,14 @@ def voigt(a,u):
     x = np.asarray(u).astype(np.float64)
     y = np.asarray(a).astype(np.float64)
 
-    w = np.array([0.462243670,   0.286675505,   0.109017206,
-                  0.0248105209,  0.00324377334, 0.000228338636,
-                  7.80255648e-6, 1.08606937e-7, 4.39934099e-10,
-                  2.22939365e-13])
+    #w = np.array([0.462243670,   0.286675505,   0.109017206,
+    #              0.0248105209,  0.00324377334, 0.000228338636,
+    #              7.80255648e-6, 1.08606937e-7, 4.39934099e-10,
+    #              2.22939365e-13])
 
-    t = np.array([0.245340708, 0.737473729, 1.23407622, 1.73853771,
-                  2.25497400,  2.78880606,  3.34785457, 3.94476404,
-                  4.60368245,  5.38748089])
+    #t = np.array([0.245340708, 0.737473729, 1.23407622, 1.73853771,
+    #              2.25497400,  2.78880606,  3.34785457, 3.94476404,
+    #              4.60368245,  5.38748089])
 
     # Hummer's Chebyshev Coefficients
     c = ( 0.1999999999972224, -0.1840000000029998,   0.1558399999965025,
@@ -255,9 +260,37 @@ def voigt(a,u):
                 yn = yn * y2
                 g = dn.astype(np.float64) * yn
                 funct = funct + q * g
-                if np.max(np.abs(g / funct)) <= 1.e-8: break
+                if np.max(np.abs(g / funct)) <= 1.e-8:
+                    break
 
     k1 = u1 - 1.12837917 * funct
     k1 = k1.astype(np.float64).clip(0)
     return k1
+
+class MetalLines:
+    """Generate metal line spectra from simulation snapshot"""
+    def __init__(self,los_table, snapshot, cloudy_dir):
+        #Get los table from group list
+        f = h5py.File(snapshot)
+        #generate metal and hydrogen spectral densities
+        #Indexing is: rho_metals [ NMETALS, NSPECTRA, NBIN ]
+        (self.rho_out, self.temp_out, self.vel_out, self.rho_metal, self.temp_metal, self.vel_metal) = SPH_Interpolate(f["PartType0"], los_table, 1024, f["Header"].attrs["BoxSize"])
+        #Generate cloudy tables
+        self.cloudy = convert_cloudy.CloudyTable(cloudy_dir)
+        self.redshift = f["Header"].attrs["Redshift"]
+
+    def get_lines(self, species, ion):
+        """Get the optical depth for a particular species number for all spectra:
+           (C, N, O, Ne, Mg, Si, Fe)
+           and ion number
+        """
+        metal_density = self.rho_metal[:, species]
+        #Use the total metallicity from summing metal species, not from the GFM_Metallicity
+        #variable as the difference is small (~ 4%)
+        tot_met = np.sum(metal_density,axis=0)
+        ions = self.cloudy.interpolate(self.redshift, tot_met, metal_density, species)
+        ion_density = ions[:,:,ion]* metal_density
+        tau_metal = compute_absorption(ion_density, self.temp_metal[:, species], self.vel_metal[:,species],line_data.lines[species+2,ion])
+        return tau_metal
+
 
