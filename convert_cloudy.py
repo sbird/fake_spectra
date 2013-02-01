Specify corrected units in convert_cloudy

convert_cloudy.py
@@ -96,6 +96,9 @@ class CloudyTable:
         self.reds = np.array([1,2,3,4,5,6])
         self.mets = np.array([-4,-3,-2,-1,0,1])
         self.dens = np.array([-6,-5,-4,-3,-2,-1,0,1,2])
+        self.species = ("He", "C", "N", "O", "Ne", "Mg", "Si", "Fe")
+        #Solar abundances from Hazy table 7.1 as Z = n/n(H)
+        self.solar = np.array([0.1, 2.45e-4, 8.51e-5, 4.9e-4, 1.e-4, 3.47e-5, 3.47e-5, 2.82e-5])
         try:
             datafile = np.load(self.savefile)
             self.table = datafile["table"]
@@ -104,14 +107,26 @@ class CloudyTable:
             self.save_file()
         self.directory = directory
 
-    def interpolate(self,red, met, rho, species, ion):
+    def ion(self,species, ion, red, met, rho):
         """Interpolate a table onto given redshift, metallicity and density for species
-        Returns a log( ionisation fraction ) """
-        red_ints = interp1d(self.reds, self.table[species,:,:,:,ion],axis = 0)
+        Returns a log( ionisation fraction ).
+        rho is the density of hydrogen in atoms / cm^3. Internally the log is taken
+        met is the mass-weighted metallicity of a species
+        specified as Z = n/ n(H), and internally converted into
+        X = log_10(Z / Z_solar) for cloudy
+        red is the redshift
+        species is the element name of the metal species we want
+        ion is the ionisation number
+        Returns the fraction of this ion in the species
+        """
+        crho = np.alog10(rho)
+        cmet = np.alog10(met/self.solar[species])
+        cspe = self.species.index(species)
+        red_ints = interp1d(self.reds, self.table[cspe,:,:,:,ion],axis = 0)
         met_ints = interp1d(self.mets, red_ints(red),axis = 0)
-        dens_ints = interp1d(self.dens, met_ints(met),axis = 0)
-        ions = dens_ints(rho)
-        return ions
+        dens_ints = interp1d(self.dens, met_ints(cmet),axis = 0)
+        ions = dens_ints(crho)
+        return 10**ions
 
     def save_file(self):
         """

spectra.py
@@ -69,7 +69,7 @@ def SPH_Interpolate(data, los_table, nbins, box):
     zz=np.array(los_table.zz, dtype=np.float32)
     axis=np.array(los_table.axis, dtype=np.int32)
     try:
-        metal_in = np.array(data["GFM_Metals"],dtype=np.float32)[:,2:]
+        metal_in = np.array(data["GFM_Metals"],dtype=np.float32)[:,1:]
         #Deal with floating point roundoff - metal_in will sometimes be negative
         metal_in[np.where(np.abs(metal_in) < 1e-10)] = 0
         (rho_HI, vel_HI, temp_HI, rho_metal, vel_metal, temp_metal) =  _SPH_Interpolate(nbins, box, pos, vel, mass, u, nh0, ne, metal_in, hh, axis, xx, yy, zz)
@@ -185,7 +185,7 @@ class MetalLines:
         self.Hz = 100.0*self.hubble * np.sqrt(Omega0/self.atime**3 + OmegaLambda)
         self.nbins = nbins
         self.xbins = np.arange(0,self.nbins)*self.box/self.nbins
-        self.species = ['C','N','O','Ne','Mg','Si','Fe']
+        self.species = ['He', 'C', 'N', 'O', 'Ne', 'Mg', 'Si', 'Fe']
         self.NumLos = np.size(los_table.axis)
         #Line data
         self.lines = line_data.LineData()
@@ -214,16 +214,14 @@ class MetalLines:
            NOTE: May wish to special-case SiIII at some point
         """
         spec_ind = self.species.index(species)
-        metal_density = self.metals[spec_ind].rho
-        #Use the total metallicity from summing metal species, not from the GFM_Metallicity
-        #variable as the difference is small (~ 4%)
-        tot_met = np.sum(metal_density,axis=0)
-        ion = self.cloudy.interpolate(self.redshift, tot_met, metal_density, spec_ind, ion)
-        #Generate density of this ion: cloudy densities are in log_10
-        ion_density = 10**ion * metal_density
+        ion_density = np.array(self.metals[spec_ind].rho)
         #Compute tau for this metal ion
         tau_metal=np.empty(np.shape(self.metals[spec_ind].rho))
         for n in np.arange(0,self.NumLos):
+            #For the density parameter use the hydrogen density at this pixel
+            #For metallicity pass the metallicity of this species and it will be converted to cloudy format
+            for i in np.arange(0,self.nbins):
+                ion_density[n,i] *= self.cloudy.ion(species, ion, self.redshift, ion_density[n,i], self.rho_H[n,i])
             tau_metal[n] = compute_absorption(self.xbins, ion_density[n], self.metals[spec_ind].vel[n], self.metals[spec_ind].temp[n], self.lines.get_line(species,1),self.Hz,self.hubble, self.box, self.atime,self.lines.get_mass(species))
         return tau_metal
 