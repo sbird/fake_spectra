53c9ed1817baa8f38886087430fefa37c3d356f5
Simeon Bird <spb@ias.edu>
2013-04-09 18:11:47 -0400

    More plotting tweaks

--- make_vel_width.py
+++ make_vel_width.py
@@ -60,6 +60,14 @@ def plot_max_col_den(sim, snap, ff=False):
     HI_col_den = np.max(hspec.get_col_density("H", 1),axis=1)
     plt.loglog(HI_col_den, metal_col_den, 'o')
 
+def plot_spectrum(sim, snap, num):
+    """Plot a spectrum"""
+    halo = "Cosmo"+str(sim)+"_V6"
+    #Load from a save file only
+    hspec = ps.PlottingSpectra(snap, base+halo, None, None)
+    tau = hspec.get_tau("Si", 2)
+    hspec.plot_spectrum(tau, num)
+
 def plot_vel_width_metcol(sim, snap, ff=False):
     """Load a simulation and plot its velocity width"""
     halo = "Cosmo"+str(sim)+"_V6"
@@ -69,6 +77,16 @@ def plot_vel_width_metcol(sim, snap, ff=False):
     hspec = ps.PlottingSpectra(snap, base+halo, None, None)
     hspec.plot_vel_width("Si", 2, met_cut = None)
     hspec.plot_vel_width("Si", 2, met_cut = 1e13, color="blue")
+    vel_data.plot_prochaska_2008_data()
+
+def plot_vel_width_DLA(sim, snap, ff=False):
+    """Plot the effect of the HI cut"""
+    halo = "Cosmo"+str(sim)+"_V6"
+    #Load from a save file only
+    hspec = ps.PlottingSpectra(snap, base+halo, None, None)
+    hspec.plot_vel_width("Si", 2, color="red", HI_cut = None)
+    hspec.plot_vel_width("Si", 2, color="blue", HI_cut = 10**20.3)
+    vel_data.plot_prochaska_2008_data()
 
 def plot_vel_col_den(sim, snap, ff=False):
     """Load a simulation and plot the metal column density vs the HI column density"""
@@ -86,37 +104,48 @@ def test_spec_resolution():
     #Do spectral resolution test
     halo = "Cosmo0_V6"
     #Higher resolution spectrum
-    hspec = ps.PlottingSpectra(68, base+halo, None, None, savefile=base+halo+"/snapdir_068/spectra1024.hdf5")
+    hspec = ps.PlottingSpectra(68, base+halo, None, None, savefile=base+halo+"/snapdir_068/spectra.hdf5")
     hspec.plot_vel_width("Si",2, color="red")
-    hspec = ps.PlottingSpectra(68, base+halo, None, None, savefile=base+halo+"/snapdir_068/spectra2048.hdf5")
-    hspec.plot_vel_width("Si",2, color="blue")
     hspec2 = ps.PlottingSpectra(68, base+halo, None, None, savefile=base+halo+"/snapdir_068/spectra4096.hdf5")
-    hspec2.plot_vel_width("Si", 2, color="black")
+    hspec2.plot_vel_width("Si", 2, color="blue", unres=10)
     del hspec2
     vel_data.plot_prochaska_2008_data()
-    plt.xlim(0, 1000)
-    plt.title("Velocity Widths at z=3")
+    plt.xlim(10, 1000)
+    plt.ylim(1e-5, 2e-2)
     save_figure(path.join(outdir,"cosmo_vel_width_z3_spectra_pix"))
     plt.clf()
 
     metal_col_den = np.max(hspec.get_col_density("Si", 2),axis=1)
     vel= hspec.vel_width(hspec.metals[("Si",2)][3])
     plt.loglog(metal_col_den, vel, 'o')
-    save_figure(path.join(outdir,"cosmo0_vel_col_spectra_pix"))
-    plt.clf()
     del hspec
 
-colors=["red", "blue", "orange", "purple"]
+colors=["red", "blue", "orange", "black"]
+
+plot_spectrum(0,60, 102)
+plt.xlim(1100, 1600)
+save_figure(path.join(outdir,"cosmo0_Si_spectrum"))
+plt.clf()
+
+#Best-fit base model
+plot_vel_width_sim(0, 60, "blue", HI_cut = 10**20.3)
+vel_data.plot_prochaska_2008_data()
+plt.xlim(10, 1000)
+plt.ylim(1e-5, 2e-2)
+save_figure(path.join(outdir,"cosmo_vel_width_z3"))
+plt.clf()
 
 test_spec_resolution()
+save_figure(path.join(outdir,"cosmo0_vel_col_spectra_pix"))
+plt.clf()
+
 #The vel widths for different simulations
-for ss in (3,2,1,0):
+for ss in (3,0):
     plot_vel_width_sim(ss, 60, colors[ss])
 
 vel_data.plot_prochaska_2008_data()
-plt.xlim(0, 1000)
-plt.title("Velocity Widths at z=3")
-save_figure(path.join(outdir,"cosmo_vel_width_z3"))
+plt.xlim(10, 1000)
+save_figure(path.join(outdir,"cosmo_feedback_z3"))
 plt.clf()
 
 #A plot of the redshift evolution
@@ -125,7 +154,7 @@ for ii in (0,1,2):
     plot_vel_width_sim(0, zz[ii], color=colors[ii])
 
 vel_data.plot_prochaska_2008_data()
-plt.xlim(0, 1000)
+plt.xlim(10, 1000)
 plt.title("Velocity Widths at z=4-2")
 save_figure(path.join(outdir,"cosmo_vel_width_zz"))
 plt.clf()
@@ -139,38 +168,26 @@ save_figure(path.join(outdir,"cosmo_rel_vel_width_z3"))
 plt.clf()
 
 for ii in (0,3):
+    #Plot effect of ignoring low column density metals
     plot_vel_width_metcol(ii, 60)
-    vel_data.plot_prochaska_2008_data()
-    save_figure(path.join(outdir,"cosmo"+str(ii)+"_vel_col_z3"))
-    plt.clf()
-
-for i in (0,1,2):
-    plot_vel_width_sim(0,zz[i],ff=True, HI_cut = 10**20.3)
-    vel_data.plot_prochaska_2008_data()
-    save_figure(path.join(outdir,"cosmo512_vel_widthz"+str(zz[i])))
+    save_figure(path.join(outdir,"cosmo"+str(ii)+"_low_metals_z3"))
     plt.clf()
 
 for ii in (0,3):
-    plot_vel_width_sim(ii,60,HI_cut = 10**20.3)
-    vel_data.plot_prochaska_2008_data()
-    save_figure(path.join(outdir,"cosmo"+str(ii)+"_rel_vel_width_DLA"))
+    #Plot effect of HI cut
+    plot_vel_width_DLA(ii,60)
+    save_figure(path.join(outdir,"cosmo"+str(ii)+"_vel_DLA"))
     plt.clf()
 
-plot_max_col_den(0, 60,ff=True)
-save_figure(path.join(outdir,"cosmo0_512z3_coldens"))
-plt.clf()
-
-plot_vel_col_den(0, 60,ff=True)
-save_figure(path.join(outdir,"cosmo0_512z3_vvcoldens"))
-plt.clf()
-
-plot_vel_width_metcol(0, 60, True)
-vel_data.plot_prochaska_2008_data()
-save_figure(path.join(outdir,"cosmo0_512_rel_vel_width_metcol"))
-plt.clf()
-
 for ii in (0,1,2,3):
+    #Plot col_density of metals vs HI
     plot_max_col_den(ii, 60)
     save_figure(path.join(outdir,"cosmo"+str(ii)+"z3_coldens"))
+    plt.clf()
+
+for ii in (0,1,2,3):
+    #Plot metal col. den vs vel width
+    plot_vel_col_den(ii, 60)
+    save_figure(path.join(outdir,"cosmo"+str(ii)+"_vel_col_z3"))
     plt.clf()
 
