5f7bcd6ad9a8697f013d182092024c468fb6d503
Simeon Bird <spb@ias.edu>
2013-04-17 12:12:33 -0400

    Random spectra across the box

---
+++ randspectra.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+"""Class to gather and analyse various metal line statistics"""
+
+import numpy as np
+import os.path as path
+import hdfsim
+import spectra
+
+class RandSpectra(spectra.Spectra):
+    """Generate metal line spectra from simulation snapshot"""
+    def __init__(self,num, base, numlos=5000, res = 1., cloudy_dir="/home/spb/codes/ArepoCoolingTables/tmp_spb/", savefile=None):
+        #Load halos to push lines through them
+        f = hdfsim.get_file(num, base, 0)
+        self.box = f["Header"].attrs["BoxSize"]
+        f.close()
+        self.NumLos = numlos
+        #All through y axis
+        axis = np.ones(self.NumLos)
+        #Sightlines at random positions
+        #Re-seed for repeatability
+        np.random.seed(23)
+        cofm = self.box*np.random.random_sample((self.NumLos,3))
+        if savefile == None:
+            savefile=path.join(self.base,"snapdir_"+str(self.num).rjust(3,'0'),"rand_spectra.hdf5")
+        spectra.Spectra.__init__(self,num, base, cofm, axis, res, cloudy_dir,savefile=savefile)
+
