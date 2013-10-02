d3bfb0d65314df39a42390dd5a7d40dd7a61b758
Simeon Bird <spb@ias.edu>
2013-10-02 15:30:43 -0400

    Allow loading of different box sizes

--- myname.py
+++ myname.py
@@ -4,11 +4,11 @@ import os.path as path
 
 base=path.expanduser("~/data/Cosmo/")
 
-def get_name(sim, ff=True):
+def get_name(sim, ff=True, box=25):
     """Get the directory for a simulation"""
     halo = "Cosmo"+str(sim)+"_V6"
     if ff:
-        halo=path.join(halo,"L25n512/output")
+        halo=path.join(halo,"L"+str(box)+"n512/output")
     else:
-        halo=path.join(halo,"L25n256")
+        halo=path.join(halo,"L"+str(box)+"256")
     return path.join(base, halo)
