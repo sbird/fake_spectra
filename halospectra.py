b122d5577a87d398633a3c2d45dafc7895f35cee
Simeon Bird <spb@ias.edu>
2016-02-02 16:45:17 -0500

    Update the rather unloved halospectra

--- halospectra.py
+++ halospectra.py
@@ -1,12 +1,13 @@
 # -*- coding: utf-8 -*-
 """Class to gather and analyse various metal line statistics"""
 
-import numpy as np
-import hdfsim
-import h5py
-import halocat
+import math
 import os.path as path
+import numpy as np
+import h5py
 import spectra
+import hdfsim
+import halocat
 try:
     xrange(1)
 except NameError:
@@ -14,8 +15,8 @@ except NameError:
 
 class HaloSpectra(spectra.Spectra):
     """Generate metal line spectra from simulation snapshot"""
-    def __init__(self,num, base, repeat = 1, minpart = 3000, res = 1., savefile="halo_spectra.hdf5", savedir=None, cdir=None):
-        if savedir == None:
+    def __init__(self,num, base, repeat = 1, min_mass = 1e9, max_mass=1e11, res = 1., offset=1., savefile="halo_spectra.hdf5", savedir=None, cdir=None):
+        if savedir is None:
             savedir = path.join(base,"snapdir_"+str(num).rjust(3,'0'))
         self.savefile = path.join(savedir,savefile)
         try:
@@ -29,10 +30,13 @@ class HaloSpectra(spectra.Spectra):
             self.OmegaM = f["Header"].attrs["Omega0"]
             self.box = f["Header"].attrs["BoxSize"]
             self.npart=f["Header"].attrs["NumPart_Total"]+2**32*f["Header"].attrs["NumPart_Total_HighWord"]
-            min_mass = self.min_halo_mass(minpart)
+            min_mass = min_mass/1e10
             f.close()
             (_, self.sub_mass, cofm, self.sub_radii) = halocat.find_wanted_halos(num, base, min_mass)
-            self.sub_cofm = np.array(cofm, dtype=np.float64)
+            ii = np.where(self.sub_mass < max_mass/1e10)
+            self.sub_mass = self.sub_mass[ii]
+            self.sub_radii = self.sub_radii[ii]
+            self.sub_cofm = np.array(cofm[ii], dtype=np.float64)
             self.NumLos = np.size(self.sub_mass)
             #All through y axis
             axis = np.ones(self.NumLos)
@@ -43,7 +47,7 @@ class HaloSpectra(spectra.Spectra):
             self.repeat = repeat
             #Re-seed for repeatability
             np.random.seed(23)
-            cofm = self.get_cofm()
+            cofm = self.get_cofm(offset)
 
         spectra.Spectra.__init__(self,num, base, cofm, axis, res, savefile=self.savefile, savedir=savedir,reload_file=True, cdir=cdir)
 
@@ -51,22 +55,21 @@ class HaloSpectra(spectra.Spectra):
         if cofm != None:
             self.replace_not_DLA(self.NumLos)
 
-    def get_cofm(self, num = None):
+    def get_cofm(self, offset=1, *, num = None):
         """Find a bunch more sightlines"""
         if num != None:
             raise NotImplementedError
         cofm = np.repeat(self.sub_cofm,self.repeat,axis=0)
         #Perturb the sightlines within a sphere of the virial radius.
-        #We want a representative sample of DLAs.
-        #maxr = self.sub_radii
+        maxr = self.sub_radii*offset
         #Generate random sphericals
-        #theta = 2*math.pi*np.random.random_sample(self.NumLos)-math.pi
-        #phi = 2*math.pi*np.random.random_sample(self.NumLos)
-        #rr = np.repeat(maxr,self.repeat)*np.random.random_sample(self.NumLos)
+        theta = 2*math.pi*np.random.random_sample(self.NumLos)-math.pi
+        phi = 2*math.pi*np.random.random_sample(self.NumLos)
+        rr = np.repeat(maxr,self.repeat)*np.random.random_sample(self.NumLos)
         #Add them to halo centers
-        #cofm[:,0]+=rr*np.sin(theta)*np.cos(phi)
-        #cofm[:,1]+=rr*np.sin(theta)*np.sin(phi)
-        #cofm[:,2]+=rr*np.cos(theta)
+        cofm[:,0]+=rr*np.sin(theta)*np.cos(phi)
+        cofm[:,1]+=rr*np.sin(theta)*np.sin(phi)
+        cofm[:,2]+=rr*np.cos(theta)
         return cofm
 
     def save_file(self):
@@ -108,8 +111,8 @@ class HaloSpectra(spectra.Spectra):
     def line_offsets(self):
         """Find the minimum distance between each line and its parent halo"""
         offsets = np.zeros(self.NumLos)
-        hcofm = np.array([self.find_associated_halo(ii)[2] for ii in xrange(self.NumLos)])
-        hrad = np.array([self.find_associated_halo(ii)[3] for ii in xrange(self.NumLos)])
+        hcofm = np.array([self.find_associated_halo(ii)[2] for ii in range(self.NumLos)])
+        hrad = np.array([self.find_associated_halo(ii)[3] for ii in range(self.NumLos)])
         hpos = self.get_spectra_proj_pos(cofm=hcofm)
         lpos = self.get_spectra_proj_pos()
         offsets = np.sqrt(np.sum((hpos-lpos)**2,axis=1))/hrad
@@ -119,6 +122,6 @@ class HaloSpectra(spectra.Spectra):
         """Do nothing - halos already loaded"""
         return
 
-    def replace_not_DLA(self, thresh=10**20.3):
+    def replace_not_DLA(self, ndla, thresh=10**20.3, elem="H", ion=1):
         """Do nothing"""
         return
