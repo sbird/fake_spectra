sbird/fake_spectra

commit 4018b6d6fdb5e2ef224cc8e717b20e4910216139

Correct the definition of the smoothing length, so that the density estimator is that used in Price 2011, eq. 6.
Before I was using a slightly different scaling for the SPH kernel,
which came with a different normalisation. The different for Arepo is
small (because I was internally consistent with this definition) but the
difference for Gadget was large, because the smoothing length was not
what I thought it was!

It differed by a factor of 2 * (4 pi/3)^1/3 ~ pi

absorption.cpp
@@ -49,9 +49,10 @@
  * If K(q) is the SPH kernel we need
  * int_zlow^zhigh K(q) dz
  *
- * Normalized such that 4 pi int_{q < 1} K(q) q^2 dq = 4 pi/3
+ * Normalized such that 4 pi int_{q < 1} K(q) q^2 dq = 1
+ * (this matches the definition used in gadget for the smoothing length!)
  * and q^2 = (b^2 + z^2)/h^2, implying that (since int_{q<1} K(q) q^2 dq = 1/32)
- * we want a normalisation of 32/3
+ * we want a normalisation of 32/(4*pi)
  *
  * */
 double sph_kern_frac(double zlow, double zhigh, double smooth, double dr2, double zrange)
@@ -74,7 +75,7 @@ double sph_kern_frac(double zlow, double zhigh, double smooth, double dr2, doubl
     double qhigh = sqrt(dr2+zhigh*zhigh)/smooth;
 
     total += sph_kernel(qhigh)/2.;
-    return 32./3.*deltaz*total;
+    return 32./4/M_PI*deltaz*total;
 }
 
 #else

test.cpp
@@ -49,24 +49,27 @@ BOOST_AUTO_TEST_CASE(check_sph_kern_frac)
     * If K(q) is the SPH kernel we need
     * int_zlow^zhigh K(q) dz
     *
-    * Normalized such that 4 pi int_{q < 1} K(q) q^2 dq = 4 pi/3
+    * Normalized such that 4 pi int_{q < 1} K(q) q^2 dq = 1
     * and q^2 = (b^2 + z^2)/h^2
     * The full range of the function is:
     * zlow = -1, zhigh = 1, coming to:
-    *  2 int_0^1 K(z) dz = 8
+    *  2 int_0^1 K(z) dz = 3/(2 pi)
     *  zrange = sqrt(smooth*smooth-dr2)
     */
-    FLOATS_NEAR_TO(sph_kern_frac(-1,1,1,0,1), 8.);
+    //Correction from the normalisation I worked out in mathematica,
+    //because the smoothing length definition changed.
+    const double corr = 3/(4*M_PI);
+    FLOATS_NEAR_TO(sph_kern_frac(-1,1,1,0,1), 3*2/M_PI);
     //Should be the same with a wide range.
-    FLOATS_NEAR_TO(sph_kern_frac(-3,10,1,0,1), 8.);
+    FLOATS_NEAR_TO(sph_kern_frac(-3,10,1,0,1), 3*2/M_PI);
     //Small variations
-    FLOATS_APPROX_NEAR_TO(sph_kern_frac(-0.15,-0.1,1,0,1), 0.489167);
+    FLOATS_APPROX_NEAR_TO(sph_kern_frac(-0.15,-0.1,1,0,1), corr*0.489167);
     //b!=0
-    FLOATS_APPROX_NEAR_TO(sph_kern_frac(0.05,0.1,1,0.4,0.774597), 0.051004);
-    FLOATS_APPROX_NEAR_TO(sph_kern_frac(0.15,0.16,1,0.8,0.447214), 0.000167423);
-    FLOATS_APPROX_NEAR_TO(sph_kern_frac(-0.05,0.1,1,0.9,0.316228), 0.00040101);
+    FLOATS_APPROX_NEAR_TO(sph_kern_frac(0.05,0.1,1,0.4,0.774597), corr*0.051004);
+    FLOATS_APPROX_NEAR_TO(sph_kern_frac(0.15,0.16,1,0.8,0.447214), corr*0.000167423);
+    FLOATS_APPROX_NEAR_TO(sph_kern_frac(-0.05,0.1,1,0.9,0.316228), corr*0.00040101);
     //Test wider range with b!=0
-    FLOATS_APPROX_NEAR_TO(sph_kern_frac(0.3,1,1,0.3,0.83666), 0.177801);
+    FLOATS_APPROX_NEAR_TO(sph_kern_frac(0.3,1,1,0.3,0.83666), corr*0.177801);
     //Check outside of range
     FLOATS_NEAR_TO(sph_kern_frac(1.5,2,1,0,1), 0);
 
@@ -90,7 +93,7 @@ BOOST_AUTO_TEST_CASE(check_compute_colden)
     test.add_colden_particle(colden, TNBINS, 0, 1,5002.5, 1);
     //Column density should be in one bin only
     //and total should be rho h int(-1,1)
-    double total = 8.;
+    double total = 8*3./(4*M_PI);
     BOOST_CHECK_EQUAL(colden[999],0);
     FLOATS_NEAR_TO(colden[1000],total);
     BOOST_CHECK_EQUAL(colden[1001],0);
@@ -128,7 +131,7 @@ BOOST_AUTO_TEST_CASE(check_compute_colden)
     //Check non-zero offset from line
     test.add_colden_particle(colden, TNBINS, 0.7, 1,4852.5, 1);
     FLOATS_NEAR_TO(colden[969],0);
-    FLOATS_APPROX_NEAR_TO(colden[970],0.0451531);
+    FLOATS_APPROX_NEAR_TO(colden[970],0.0451531*3/(4*M_PI));
     BOOST_CHECK_EQUAL(colden[971],0);
     nonzero.insert(970);
     //Offset enough to not add anything

hsml.py
@@ -1,36 +1,28 @@
 # -*- coding: utf-8 -*-
-"""A small module for computing the smoothing length of a particle simulation.
-(Non-trivial in Arepo)"""
+"""A small module for computing the smoothing length of a Gadget/Arepo simulation."""
 
-from math import pi
 import numpy as np
-import numexpr as ne
-
-#To get these factors as floats for numexpr
-scal = np.float32(3./4./pi)
-poww = np.float32(1./3.)
-
 
 def get_smooth_length(bar):
     """Figures out if the particles are from AREPO or GADGET
     and computes the smoothing length.
     Note the Volume array in HDF5 is comoving and this returns a comoving smoothing length
-    If we are Arepo, this smoothing length is  cell radius, where
-    cell volume = 4/3 π (cell radius) **3 and cell volume = mass / density
+    The SPH kernel definition used in Gadget (Price 2011: arxiv 1012.1885)
+    gives a normalisation so that rho_p = m_p / h^3
+    So the smoothing length for Arepo is Volume^{1/3}
+    For gadget the kernel is defined so that the smoothing length is 2*h.
     Arguments:
         Baryon particles from a simulation
     Returns:
         Array of smoothing lengths in code units.
     """
     #Are we arepo? If we are a modern version we should have this array.
-    if 'Volume' in bar.keys():
-        volume=np.array(bar["Volume"],dtype=np.float32)
-        radius = ne.evaluate("(scal*volume)**poww")
-    elif 'Number of faces of cell' in bar.keys():
-        rho=np.array(bar["Density"])
-        mass=np.array(bar["Masses"])
-        radius = ne.evaluate("(scal*mass/rho)**poww")
-    else:
-        #If we are gadget, the SmoothingLength array is actually the smoothing length.
-        radius=np.array(bar["SmoothingLength"])
+    try:
+        radius = np.cbrt(bar["Volume"], dtype=np.float32)
+    except KeyError:
+        #If we don't have a Volume array we are gadget, and
+        #the SmoothingLength array is actually the smoothing length.
+        #There is a different kernel definition, as in gadget the kernel goes from 0 to 2,
+        #whereas I put it between zero and 1.
+        radius=np.array(bar["SmoothingLength"],dtype=np.float32)/2
     return radius

main.cpp
@@ -214,6 +214,8 @@ int main(int argc, char **argv)
            for(int ii = 0; ii< Npart; ii++){
              P.Mass[ii] *= P.fraction[ii]*XH;
              P.temp[ii] = compute_temp(P.U[ii], P.Ne[ii], XH);
+             /*Correct the smoothing length kernel to match our definition instead of gadget's.*/
+             P.h[ii]/=2;
            }
           /*Do the hard SPH interpolation*/
           pint.compute_tau(tau_H1,P.Pos, P.Vel, P.Mass, P.temp, P.h, Npart);

commit 11fd6ddec7bf86067ced4b0c058891718ac2f4b7

Adjust the optical depth to match the new SPH kernel.
This fixes a bug introduced in 4018b6d6fdb5e2ef224cc8e717b20e4910216139
where the SPH kernel length was changed. We changed the normalisation of
the SPH kernel, but omitted to change it for the optical depth. Solve
this by normalising inside sph_kernel instead, so it is only in one
place. Also adjust the tests.

absorption.cpp
@@ -75,7 +75,7 @@ double sph_kern_frac(double zlow, double zhigh, double smooth, double dr2, doubl
     double qhigh = sqrt(dr2+zhigh*zhigh)/smooth;
 
     total += sph_kernel(qhigh)/2.;
-    return 32./4/M_PI*deltaz*total;
+    return deltaz*total;
 }
 
 #else

singleabs.h
@@ -12,12 +12,13 @@
  * This changes the values of the constants, but the kernel value is the same.*/
 inline double sph_kernel(const double q)
 {
+    const double norm = 32./4/M_PI;
     if (q >= 1)
         return 0;
     if(q<0.5)
-        return 1-6*q*q+6*q*q*q;
+        return norm*(1-6*q*q+6*q*q*q);
     else
-        return 2*pow(1.- q,3);
+        return norm*(2*pow(1.- q,3));
 }
 
 
@@ -127,7 +128,7 @@ class SingleAbsorber
                 const double T0 = vdiff/btherm;
                 total+=sph_kernel(q)*profile(T0,aa);
             }
-            return 32./3.*deltav*total;
+            return deltav*total;
         }
 
         const double btherm;

test.cpp
@@ -36,11 +36,12 @@
 BOOST_AUTO_TEST_CASE(check_sph_kern)
 {
     //First check the kernel is working
+    const double norm = 32./4/M_PI;
     FLOATS_NEAR_TO(sph_kernel(1),0);
-    FLOATS_NEAR_TO(sph_kernel(0),1);
-    FLOATS_NEAR_TO(sph_kernel(0.5),0.25);
-    FLOATS_NEAR_TO(sph_kernel(0.25),0.71875);
-    FLOATS_NEAR_TO(sph_kernel(0.75),0.03125);
+    FLOATS_NEAR_TO(sph_kernel(0),norm);
+    FLOATS_NEAR_TO(sph_kernel(0.5),0.25*norm);
+    FLOATS_NEAR_TO(sph_kernel(0.25),0.71875*norm);
+    FLOATS_NEAR_TO(sph_kernel(0.75),0.03125*norm);
 }
 
 BOOST_AUTO_TEST_CASE(check_sph_kern_frac)
@@ -304,26 +305,29 @@ BOOST_AUTO_TEST_CASE(check_single_absorber)
     //Integrals evaluated numerically as usual with mathematica
     //H at 20000 K.
     double bb = 0.128557*sqrt(2e4/1);
+    //Correction from the normalisation I worked out in mathematica,
+    //because the smoothing length definition changed.
+    const double corr = 3/(4*M_PI);
     //First test not offset
     SingleAbsorber sing(bb,0,10,1.e-4);
     //Absorption at the origin
-    FLOATS_NEAR_TO(sing.tau_kern_outer(0,0),78.0409);
-    FLOATS_NEAR_TO(sing.tau_kern_outer(5,5),72.6216);
-    FLOATS_NEAR_TO(sing.tau_kern_outer(10,10),58.5185);
-    FLOATS_NEAR_TO(sing.tau_kern_outer(20,20),24.6696);
+    FLOATS_NEAR_TO(sing.tau_kern_outer(0,0),corr*78.0409);
+    FLOATS_NEAR_TO(sing.tau_kern_outer(5,5),corr*72.6216);
+    FLOATS_NEAR_TO(sing.tau_kern_outer(10,10),corr*58.5185);
+    FLOATS_NEAR_TO(sing.tau_kern_outer(20,20),corr*24.6696);
     //Try different smoothing length
     SingleAbsorber sing2(bb,0,2,1.e-4);
-    FLOATS_NEAR_TO(sing2.tau_kern_outer(5,5),14.8203);
+    FLOATS_NEAR_TO(sing2.tau_kern_outer(5,5),corr*14.8203);
     //Offset from the sightline
     SingleAbsorber sing3(bb,25,10,1.e-4);
-    FLOATS_APPROX_NEAR_TO(sing3.tau_kern_outer(0,0),18.218);
-    FLOATS_APPROX_NEAR_TO(sing3.tau_kern_outer(5,5),16.9436);
+    FLOATS_APPROX_NEAR_TO(sing3.tau_kern_outer(0,0),corr*18.218);
+    FLOATS_APPROX_NEAR_TO(sing3.tau_kern_outer(5,5),corr*16.9436);
     //Check integrating over a range in v
     //Carbon
     bb = 0.128557*sqrt(2e4/16);
     SingleAbsorber sing4(bb,0,5,1.e-6);
-    FLOATS_APPROX_NEAR_TO(sing4.tau_kern_outer(0,10),16.0403);
-    FLOATS_APPROX_NEAR_TO(sing4.tau_kern_outer(-5,5),27.1978);
+    FLOATS_APPROX_NEAR_TO(sing4.tau_kern_outer(0,10),corr*16.0403);
+    FLOATS_APPROX_NEAR_TO(sing4.tau_kern_outer(-5,5),corr*27.1978);
 }
 
 #define  BOLTZMANN  1.3806504e-16  /*  ergs K-1 or cm2 g s-2 K-1 */
@@ -355,9 +359,9 @@ BOOST_AUTO_TEST_CASE(check_add_tau)
     //Symmetric function
     FLOATS_NEAR_TO(tau[1001],tau[999]);
     //Check tail
-    FLOATS_NEAR_TO(tau[0],SA(0,5002.5, 1e-3*rscale));
-    FLOATS_NEAR_TO(tau[1],SA(1,5002.5, 1e-3*rscale));
-    FLOATS_NEAR_TO(tau[2],SA(2,5002.5, 1e-3*rscale));
+    FLOATS_NEAR_TO(tau[400],SA(400,5002.5, 1e-3*rscale));
+    FLOATS_NEAR_TO(tau[401],SA(401,5002.5, 1e-3*rscale));
+    FLOATS_NEAR_TO(tau[402],SA(402,5002.5, 1e-3*rscale));
     //Reset tau
     memset(tau, 0, TNBINS*sizeof(double));
     //Check peculiar vel dependence: this should just induce an offset