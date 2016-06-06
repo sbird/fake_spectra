9e379098cbd832b1799739ee191954826c1ee57a
Simeon Bird <sbird4@jhu.edu>
2016-06-06 14:23:53 -0400

    Make the flux power return the frequency bins as well

--- spectra.py
+++ spectra.py
@@ -1253,13 +1253,22 @@ class Spectra(object):
         assert np.shape(delta_flux) == (self.NumLos, self.nbins)
         df_hat = np.fft.rfft(delta_flux,axis=1)
         flux_power = np.real(df_hat)**2 + np.imag(df_hat)**2
-        #Normalise the FFT (do I need to do this?)
+        #Normalise the FFT
         flux_power /= self.nbins**2
         assert np.shape(flux_power) == (self.NumLos, self.nbins//2 + 1)
         #Average over all sightlines
         avg_flux_power = np.mean(flux_power, axis=0)
+        #Get the frequency component
+        kf = np.fft.rfftfreq(self.nbins)
+        #Units:
+        #The largest frequency scale is the velocity scale of the box,
+        #not 1/nbins as rfftfreq gives.
+        scale= self.nbins/self.vmax
+        #Adjust Fourier convention.
+        kf *= 2.0*math.pi * scale
         assert np.shape(avg_flux_power) == (self.nbins//2 + 1,)
-        return avg_flux_power
+        avg_flux_power /= scale  #*((1+zz)/3.2)**3
+        return kf,avg_flux_power
 
 def combine_regions(condition, mindist=0):
     """Combine contiguous regions that are shorter than mindist"""
