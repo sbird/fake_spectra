b3d2400998f4e6c18a4e29b01eb7f6adab78afd3
Simeon Bird <spb@ias.edu>
2016-07-15 15:50:59 -0400

    Split out the unit system into a different file

--- spectra.py
+++ spectra.py
@@ -34,6 +34,7 @@ from scipy.ndimage.filters import gaussian_filter1d
 import hsml
 import gas_properties
 import line_data
+import unitsystem
 import hdfsim
 import subfindhdf
 import voigtfit
@@ -54,55 +55,6 @@ try:
 except NameError:
     xrange = range
 
-class UnitSystem(object):
-    """Class to store the various physical constants and units that are relevant here. Factored out of Spectra."""
-    def __init__(self, UnitMass_in_g=1.989e43, UnitLength_in_cm=3.085678e21):
-        #Internal gadget mass unit: 1e10 M_sun/h in g/h
-        self.UnitMass_in_g = UnitMass_in_g
-        #Internal gadget length unit: 1 kpc/h in cm/h
-        self.UnitLength_in_cm = UnitLength_in_cm
-        #Speed of light in cm/s
-        self.light = 2.99e10
-        #proton mass in g
-        self.protonmass=1.67262178e-24
-        #Newton's constant in cm^3/g/s^2
-        self.gravcgs = 6.674e-8
-        #h * 100 km/s/Mpc in h/s
-        self.h100=3.2407789e-18
-
-    def absorption_distance(self, speclen, red):
-        """
-        Compute X(z), the absorption distance per sightline (dimensionless)
-        X(z) = int (1+z)^2 H_0 / H(z) dz
-        When dz is small, dz ~ H(z)/c dL, so
-        X(z) ~ (1+z)^2 H_0/c dL
-        Arguments:
-            speclen - spectral length (usually box size in comoving kpc/h)
-            red - redshift
-        """
-        #Units: h/s   s/cm                 kpc/h      cm/kpc
-        return self.h100/self.light*speclen*self.UnitLength_in_cm*(1+red)**2
-
-    def redshift_distance(self, speclen, red,omegam0):
-        """Compute dz over the box, dz = H(z)/c dL
-        Arguments:
-            speclen - spectral length (usually box size in comoving kpc/h)
-            red - redshift
-        """
-        #Units: h/s   s/cm                 kpc/h      cm/kpc
-        return self.hubble(red, omegam0)/self.light*speclen*self.UnitLength_in_cm
-
-    def hubble(self, z, omegam0):
-        """Hubble parameter"""
-        return self.h100*np.sqrt(omegam0*(1+z)**3 + (1-omegam0))
-
-    def rho_crit(self, hubble):
-        """Get the critical density at z=0 in units of g cm^-3"""
-        #H in units of 1/s
-        h100=self.h100*hubble
-        rho_crit=3*h100**2/(8*math.pi*self.gravcgs)
-        return rho_crit
-
 class Spectra(object):
     """Class to interpolate particle densities along a line of sight and calculate their absorption
         Arguments:
@@ -121,7 +73,7 @@ class Spectra(object):
         if units != None:
             self.units = units
         else:
-            self.units = UnitSystem()
+            self.units = unitsystem.UnitSystem()
         #Empty dictionary to add results to
         self.tau_obs = {}
         self.tau = {}
