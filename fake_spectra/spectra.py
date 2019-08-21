47c066341019077bdb3f976448fcc3b344d5066c
Simeon Bird <sbird@ucr.edu>
2019-08-21 13:52:19 -0700

    Fix the case where there are no particles near the sightlines for the first examined segment

--- fake_spectra/spectra.py
+++ fake_spectra/spectra.py
@@ -403,9 +403,12 @@ class Spectra(object):
 
     def _interpolate_single_file(self,nsegment, elem, ion, ll, get_tau, load_all_data_first=False):
         """Read arrays and perform interpolation for a single file"""
-        (pos, vel, elem_den, temp, hh, amumass) = self._read_particle_data(nsegment, elem, ion,get_tau)
+        for nfirstseg in range(1, self.snapshot_set.get_n_segments()):
+            (pos, vel, elem_den, temp, hh, amumass) = self._read_particle_data(nsegment, elem, ion,get_tau)
+            if amumass is not False:
+                break
         if load_all_data_first:
-            for nseg in range(1, self.snapshot_set.get_n_segments()):
+            for nseg in range(nfirstseg, self.snapshot_set.get_n_segments()):
                 (pos_, vel_, elem_den_, temp_, hh_, amumass_) = self._read_particle_data(nseg, elem, ion, get_tau)
                 if amumass_ is False:
                     continue
