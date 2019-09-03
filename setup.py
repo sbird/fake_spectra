80ad735c05fe7c40478358ce7002f409f431825d
Simeon Bird <sbird@ucr.edu>
2019-09-02 16:37:07 -1000

    Bump version

--- setup.py
+++ setup.py
@@ -122,7 +122,7 @@ cmodule = [
 
 setup(
     name="fake_spectra",
-    version='2.0.7',
+    version='2.0.8',
     author="Simeon Bird",
     author_email="spb@ias.edu",
     #Use the subclass which adds openmp flags as appropriate
