4d377fe8cc66b46a0208950b20ac48b4ae157a8a
P.R. Upton Sanderbeck <phoebeu@ucr.edu>
2019-10-08 14:01:13 -0700

    Update singleabs.h

--- fake_spectra/singleabs.h
+++ fake_spectra/singleabs.h
@@ -28,18 +28,19 @@ inline double sph_cubic_kernel(const double q)
 /* The normalized quintic kernel from Price 2011: arxiv 1012.1885 , eq. 8
  * We define the support over h < 1, rather than h < 3 used there.
  * This changes the values of the constants, but the kernel value is the same.*/
-inline double sph_kernel(const double q)
+inline double sph_quintic_kernel(const double q)
 {
     const double norm = 9./40/M_PI;
     if (q >= 1)
         return 0;
     if(q < (1./3))
         return norm*6*(11-90*q*q+405*q*q*q*q-405*q*q*q*q*q);
-    if(q < (2/3) && q >= (1/3))
+    if(q < (2./3) && q >= (1./3))
         return norm*(pow(3.-3*q,5)-6*pow(2.-3*q,5));        
     else
         return norm*(243*pow(1.- q,5));
 }
+
 
 
 
