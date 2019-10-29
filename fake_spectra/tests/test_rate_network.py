7bf1de8443e8a7b403e0a4f062eef40c8fa8891e
Simeon Bird <sbird@ucr.edu>
2019-10-28 17:39:22 -0700

    Make the unit tests faster for travis

--- fake_spectra/tests/test_rate_network.py
+++ fake_spectra/tests/test_rate_network.py
@@ -110,7 +110,6 @@ def testRateNetworkGas():
     elim = (np.log(20), np.log(3e6))
     randd = (dlim[1] - dlim[0]) * np.random.random(size=2000) + dlim[0]
     randi = (elim[1] - elim[0]) * np.random.random(size=2000) + elim[0]
-    gasprop.build_interp(dlim, elim)
     spl = _interpolate_2d(randd.astype(np.float32), randi.astype(np.float32), gasprop.densgrid, gasprop.ienergygrid, gasprop.lh0grid)
     rate = np.array([np.log(gasprop.rates.get_neutral_fraction(np.exp(dd), np.exp(ii))) for dd, ii in zip(randd, randi)])
     assert np.all(np.abs(spl - rate) < 1e-5)
