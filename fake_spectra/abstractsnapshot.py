2a3b2c93976782be4fe1060960ad25556311d548
Simeon Bird <sbird@ucr.edu>
2020-07-02 11:04:22 -0700

    Change default kernel for Arepo back to tophat rather than attempting a mesh reconstruction. I was getting too many obscure bug reports about the mesh.

--- fake_spectra/abstractsnapshot.py
+++ fake_spectra/abstractsnapshot.py
@@ -272,9 +272,9 @@ class HDF5Snapshot(AbstractSnapshot):
         if "SmoothingLength" in self._f_handle["PartType0"].keys():
             return 1
         #We are Arepo if there is no smoothing length.
-        #return 2 for using the Voronoi mesh. Otherwise return 0 for the tophat kernel.
-        return 2
-        #return 0
+        #return 0 for the tophat kernel. 2 is also an option,
+        #but has too many edge cases to be the default.
+        return 0
 
 class BigFileSnapshot(AbstractSnapshot):
     """Specialised class for loading HDF5 snapshots"""
