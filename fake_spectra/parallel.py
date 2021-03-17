5adae359681247c9f2f55bd66d78d261922a4f86
Mahdi Qezlou <mahdiqezlou@gmail.com>
2021-03-17 00:18:03 -0700

    some tweaks on parallel.py

--- fake_spectra/parallel.py
+++ fake_spectra/parallel.py
@@ -1,38 +1,43 @@
-import fake_spectra
-#print("fake_spectra imported")
-from fake_spectra.randspectra import RandSpectra
-#print("haloassigned_spectra imported")
-from fake_spectra.plot_spectra import PlottingSpectra
-#print("PlottingSpectra imported")
+import fake_spectra_parallel
+from fake_spectra_parallel.randspectra import RandSpectra
+from fake_spectra_parallel.plot_spectra import PlottingSpectra
+from fake_spectra_parallel.ratenetworkspectra import RateNetworkGas
 from mpi4py import MPI
 import numpy as np
 import time
+import argparse
 
-comm = MPI.COMM_WORLD
-rank = comm.Get_rank()
-size = comm.Get_size()
+def get_spec(num, base, savefile, savedir, res, numlos, thresh):
+   comm = MPI.COMM_WORLD
+   rank = comm.Get_rank()
+   size = comm.Get_size()
 
-tss = time.asctime()
-print('Rank =', rank, 'started at :', tts, flush=True)
+   tss = time.asctime()
+   print('Rank =', rank, 'started!', tss, flush=True)
+   
+   rr = RandSpectra(num = num, numlos=numlos, ndla=numlos, thresh=thresh, res=res, base= base , MPI = MPI, comm = comm, savedir=savedir, savefile='spectra_'+str(rank)+'.hdf5', gasprop=RateNetworkGas, gasprop_args={"selfshield":True, "treecool_file":"./TREECOOL_ep_2018p", "cool":"KWH","recomb":"Cen92"}, kernel='tophat')
 
-numlos = 10000
-ndla = 1000
-thresh_t = 10**20.3
-### the array to store added column density in
+   rr.get_tau("H",1,1215)
+   rr.get_col_density("H",1)
+   rr.save_file()
 
-rank_str = str(rank)
-rr = RandSpectra(34, "/rhome/mqezl001/bigdata/TNG/TNG100-1/output/snapdir_034/" +rank_str + "/", MPI, comm,  thresh = 0.0, kernel='tophat',ndla = 1000, numlos=1000,savedir="/rhome/mqezl001/bigdata/TNG/TNG100-1/postprocessing/randspectra/Snap_034/parallel", savefile="spectra_34."+rank_str+".hdf5")
+   tsd = time.asctime() 
+   print('Rank = ', rank, 'is done !', tsd, flush=True)
+   del rr
 
-#### Calculate spectra for 100 hdf5 files
-rr.get_tau("H",1,1215)
-#Lyman-beta
-rr.get_tau("H",1,1025)
-rr.get_col_density("H",1)
-rr.get_col_density("H",-1)
-rr.get_col_density("Z", -1)
+if __name__ == '__main__':
 
-rr.save_file()
-del rr
+   parser = argparse.ArgumentParser()
+   parser.add_argument('-num', type=int, required=True)
+   parser.add_argument('-base', type=str, required=True)
+   parser.add_argument('-savedir', type=str, required=True)
+   parser.add_argument('-res',  type=float, required=True)
+   parser.add_argument('-numlos', type=int, required=True)
+   parser.add_argument('-thresh',  type=float, required=True)
 
-tsd = time.asctime()
-print('Rank =', rank, 'was Done at :', tsd, flush = True)
+   
+   args = parser.parse_args()
+
+   get_spec(num=args.num, base=args.base, savedir=args.savedir, res=args.res, numlos=args.numlos, thresh=args.thresh)
+
+
