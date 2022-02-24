aff2139d75c7618d5a17971b80a66fc65ec1f1a6
Mahdi Qezlou <mahdiqezlou@gmail.com>
2022-02-24 10:16:27 -0800

    Simple rtn from Schmidt+2019
    - Assuming nHeII/nHe = nHII/nH and nHeIII=0
    - It effects ne calculations

--- fake_spectra/ratenetworkspectra.py
+++ fake_spectra/ratenetworkspectra.py
@@ -38,7 +38,12 @@ class RateNetworkGas(gas_properties.GasProperties):
         """Compute temperature (in K) from internal energy using the rate network."""
         temp, ii2, density, ienergy = self._get_interp(part_type, segment, nhi=False)
         if np.size(ii2) > 0:
-            temp[ii2] = self.rates.get_temp(density[ii2], ienergy[ii2])
+            if self.sf_neutral:
+                #temp[ii2] = self.rates._get_temp(np.zeros_like(ii2), ienergy[ii2])
+                #print('temp[ii2], max and min', np.max(temp[ii2]), np.min(temp[ii2]), flush=True)
+                temp[ii2] = 1e4*np.ones_like(ii2)
+            else:
+                temp[ii2] = self.rates.get_temp(density[ii2], ienergy[ii2])
         assert np.all(np.logical_not(np.isnan(temp)))
         return temp
 
