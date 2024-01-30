da341bca5b43a2e3dd46093918b62ff52a2cd7fc
Sum Qezlou <mqezl001@ucr.edu>
2024-01-30 10:23:06 -0800

    Compute 3d power (#68)
    * using nbodykit to get 3D power
    - It is not yet fully developed
    - I need to undestand the nbodykit's calculations first
    - So, I will compare my won calcs with this first in a few test cases
    
    * computing P(k,mu)
    - It computes the P(k.mu) using `nbodykit`, it is just using numpy fft, but does the binning ofthe  3D pk in (k.mu) space neatly
    - I have tested the pk3de using numpy to make sure the units are alright.
    - Now, the units are in boxsize units, so [P(k,mu)] = h/cKpc
    
    * P(k,mu): CIC interpolation
    - If the resolution along the line-of-sight is better tahn the transverse, we need to degrade that along line-of-sight to match that along the  transverse direction
    - Previous comment did this by linear interpolation which hugely altered the 1D power
    - Now, we use Cloud-In-Cell interpolation. The CICd spectra are now off by ~ 5%across almost all scales.
    - That slight difference should not matter much, as we will probably ingore the mu=1 (1d power) to avoid the systematics
    - This difference could be related to rescaling the mean flux. We need to check that if mu=1 is interesting for us.
    
    * remove `window`for 3d power
    - We do not do any coorection to acount for the effect of window function on the power spectrumin 3D, so deleting the if statement
    
    * pass a seperate MPI comm for nbodykit in 3d power
    - It is introduced for the 3D power emulator. It makes sure the nbodkit is run in serial and the parallel processing is done with the emulator.
    - Does not effect other parts of the code

--- fake_spectra/fluxstatistics.py
+++ fake_spectra/fluxstatistics.py
@@ -5,9 +5,17 @@ Useful for lyman alpha forest work."""
 
 import math
 import numpy as np
-from scipy.optimize import brentq
-
+from datetime import datetime
 from ._spectra_priv import _rescale_mean_flux
+
+# You need `nbodykit` only if you want to compute the 3D power spectrum with `flux_power_3d`
+try :
+    from nbodykit.lab import FFTPower
+    from nbodykit.source.catalog import ArrayCatalog
+    from nbodykit import setup_logging
+    from nbodykit import CurrentMPIComm
+except ImportError:
+    pass
 
 def obs_mean_tau(redshift):
     """The mean flux from 0711.1862: effective optical depth is (0.0023±0.0007) (1+z)^(3.65±0.21)
@@ -97,6 +105,92 @@ def flux_power(tau, vmax, spec_res = 8, mean_flux_desired=None, window=False):
     if window and spec_res > 0:
         mean_flux_power /= _window_function(kf, R=spec_res, dv=vmax/npix)**2
     return kf,mean_flux_power
+
+def _3d_powerspectrum(dflux_mesh, boxsize, los, dk=None, Nmu=10):
+    """Compute the 3D power spectrum of the input using nbodykit
+    Parameters:
+    dfux_mesh - 3D array of flux variations, type is `mesh` in `nbodykit`
+    boxsize - size of the box in units of interest (eg, comoving cMpc/h), 
+                the units of the 3d power spectrum, i.e. P(k,mu), will be in these units
+    los - line of sight direction, i.e. [0,0,1] for z-axis
+    dk - bin width in k
+    Nmu - number of mu bins
+    Returns:
+    power - a dictionary with the p(k,mu) and the k and mu bins, keys:['power','k','mu']
+    """
+    power = FFTPower(dflux_mesh, BoxSize=boxsize, 
+                     mode='2d', los= los, dk=dk, 
+                     Nmu=Nmu)
+    return power.power
+
+def flux_power_3d(comm_nbodykit, tau, boxsize, mean_flux_desired=None, dk=None, Nmu=10, quiet=True):
+    """Get the power spectrum of (variations in) the flux in 3D which is binned in (k,mu).
+        This is: P_3D(k) = <d_F d_F>
+                 d_F = e^-tau / mean(e^-tau) - 1
+                 Then we bin P_3D(k) in k and mu.
+        If mean_flux_desired is set, the spectral optical depths will be rescaled
+        to match the desired mean flux.
+        We compute the power spectrum along each sightline and then average the result.
+        Arguments:
+        comm_nbodykit: MPI communicator for nbodykit, I prefer to have one communicator for each process, i.e.
+                        turning off parallel processing in nbodykit cause it is already fast enough. 
+                        You can set it as None if parallelism is not a concern to you.
+            tau - optical depths. Shape is (NumLos, npix)
+            mean_flux_desired - Mean flux to rescale to.
+            dk - bin width in k, units of h/cMpc
+            Nmu - number of mu bins, default is 10 which is usually used in 3D correlation function
+            quiet - if you want to see ``nbodykit`'s loggings
+
+        Returns:
+            k, mu - the k and mu bins of the power spectrum
+            flux_power - flux power spectrum in `boxsize` units
+            Note: The first row corresponds to k=0, so you can remove it later
+    """
+    with CurrentMPIComm.enter(comm_nbodykit):
+        scale = 1.
+        if mean_flux_desired is not None:
+            scale = mean_flux(tau, mean_flux_desired)
+            #print("rescaled: ",scale,"frac: ",np.sum(tau>1)/np.sum(tau>0))
+        else:
+            mean_flux_desired = np.mean(np.exp(-tau))
+        (nspec, npix) = np.shape(tau)
+        nt = np.sqrt(nspec/3).astype(int)
+        x, y, z = np.meshgrid(np.arange(nt), np.arange(nt), np.arange(npix), indexing='ij')
+        x = x*boxsize/nt
+        y = y*boxsize/nt
+        z = z*boxsize/npix
+        coords = np.vstack((x.ravel(), y.ravel(), z.ravel())).T
+        for i in range(3):
+            if not quiet:
+                print(f'Interpolating the spectra along the perp direction | {datetime.now()}', flush=True)
+            end = min((i+1)*nspec//3, nspec)
+            #Interpolate onto a regular grid
+            if not quiet:
+                setup_logging()
+            cat = ArrayCatalog({'Position': coords, 'Weight': tau[i*nspec//3:end].ravel()})
+            mesh = cat.to_mesh(Nmesh=[nt, nt, nt], BoxSize=boxsize, weight='Weight', resampler='cic')
+            get_df = lambda x, v: np.exp(-scale*v)/mean_flux_desired - 1
+            mesh = mesh.apply(get_df, mode='real', kind='index')
+            # Calculate flux power for the interpolated flux field
+            if not quiet:
+                print(f'Calculating the 3D power spectrum for axis {i} | {datetime.now()}', flush=True)
+            los = [0, 0, 0]
+            los[i] = 1
+            power = _3d_powerspectrum(mesh, boxsize=boxsize, los=los, dk=dk, Nmu=Nmu)
+            #The units of the P(k,mu) is same as the `boxsize` argument
+            if i==0:
+                mean_flux_power = power['power']
+            else:
+                mean_flux_power += power['power']
+        # Avergaing the 3D power spectrum obtained with the spectra along the 3 axes
+        mean_flux_power/= 3
+        # nobodykit calculates the power in boxsize unit
+        k = power['k']
+        mu = power['mu']
+        # We do not do any window correction along los or the transverse directions
+        # We have seen this effect been marginal for the 1D power spectrum becase
+        # the spatial resolution is much larger than the observations
+        return k, mu, mean_flux_power
 
 def _flux_power_bins(vmax, npix):
     """
